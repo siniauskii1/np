2a8575f6640d4d29f019c21f0453687e755a1a46
siniauskii1 <66171894+siniauskii1@users.noreply.github.com>
2022-02-21 01:38:10 +0300

    Update maun.py

--- maun.py
+++ maun.py
@@ -1,7 +1,6 @@
-from ServerKeeper import WatchDog
+
 import  requests
-bd = WatchDog("gretteret")
 json = {"token": 432424, "problem": "HCk", "cpu": 3, "memory": 5}
 url = "https://watchdogserver.herokuapp.com/HealthChecker"
 requests.post(url,json)
-bd.Start_watching("main.py")
+
