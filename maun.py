4707036e6bcea7b48a7d812aad9a5dc0e1e444ae
siniauskii1 <66171894+siniauskii1@users.noreply.github.com>
2022-02-21 15:09:58 +0300

    Update maun.py

--- maun.py
+++ maun.py
@@ -1,7 +1,6 @@
 import  requests
 from fake_useragent import UserAgent
 ua = UserAgent()
-json = {"token": 432424, "problem": "HCk", "cpu": 3, "memory": 5}
 url = "https://watchdogserver.herokuapp.com/HealthChecker"
-requests.post(url=url,json=json, headers={'user-agent': f'{ua.random}'}))
+requests.post(url=url,headers={'user-agent': f'{ua.random}'}))
 
